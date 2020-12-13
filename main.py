da81aaf8919bc045304165a93498ab749a008fe2
lNsufficient <edvard.jo@gmail.com>
2020-12-13 12:12:55 +0100

    Read file and save synonyms

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from urllib.request import urlopen
 from queue import LifoQueue
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
+import sys
+import os
 
-class synonym_website():
+class lookup_website():
     def __init__(self):
         pass
 
@@ -22,9 +25,12 @@ class synonym_website():
         string = re.sub(r";,", ";", string)
         string = re.sub(r"(^,)|(,$)", "", string)
         return string
+    
+    #TODO: keep track of last call timestamp to prevent website block
+    # - if they suspect a bot is being used they might block it.
         
 
-class synonymer_dot_se(synonym_website):
+class synonymer_dot_se(lookup_website):
     def __init__(self):
         super(synonymer_dot_se, self).__init__()
         self.print_next_end_tag = False
@@ -46,10 +52,28 @@ class synonymer_dot_se(synonym_website):
         synonyms = self.strip_whitespaces(dictDefaultBodyText)
         return synonyms
         #print(website_parsed)
-    
-word = "flor"
-synonymer_se = synonymer_dot_se()
-synonyms = synonymer_se.lookup_word(word)
-print(synonyms)
-#print(synonyms)
 
+#TODO: class wikipedia (ingress), google (first result)
+
+
+if __name__ == "__main__":
+    print("test")
+    input_file = sys.argv[1] #TODO: replace using click()
+    df = pd.read_csv(input_file) #TODO: csv parser and don't import pandas
+    #don't use df.apply since pandas will be removed.
+    list_of_dicts = df.to_dict('records')
+    synonymer_se = synonymer_dot_se()
+    print(list_of_dicts)
+    word_column = "quote" #TODO: Automatic
+    synonym_column = "synonyms"
+    for row in list_of_dicts:
+        word = row[word_column] 
+        try:
+            #TODO: Append to row instead if it exists.
+            row[synonym_column] = synonymer_se.lookup_word(word)
+        except:
+            row[synonym_column] = "failed looking up: " + str(word)
+    df_updated = pd.DataFrame(list_of_dicts)
+    print(df_updated)
+    #TODO: csv parser and don't import pandas
+    df_updated.to_csv(os.path.splitext(input_file)[0]+"_with_synonyms.csv")
