4d3be912b9eddfca11e14f77d9eb2e844fe7fecb
lNsufficient <edvard.jo@gmail.com>
2020-12-13 11:34:14 +0100

    Find synonym

--- main.py
+++ main.py
@@ -1,48 +1,55 @@
 from urllib.request import urlopen
-from html.parser import HTMLParser
+from queue import LifoQueue
+from bs4 import BeautifulSoup
+import re
 
-class synonym_website(HTMLParser):
+class synonym_website():
     def __init__(self):
-        print("inside super init")
-        HTMLParser.__init__(self)
+        pass
 
     def lookup_synonym(self, word):
         pass
 
+    
     def fetch_website_text(self, url):
         with urlopen(url) as f:
             text = f.read()
         return text
 
-    #HTML Parsing related functions, handles:
-    def read(self, data):
-        print(data)
-        print("\n\n")
-        print(type(data))
-        self.reset()
-        self.feed(data)
-
-    def handle_starttag(self, tag, attrs):
-        print("Encountered start of tag: ", tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered end of tag:", tag)
+    def strip_whitespaces(self, string):
+        string = re.sub(r"\s", ",", string)
+        string = re.sub(r",+", ",", string)
+        string = re.sub(r";,", ";", string)
+        string = re.sub(r"(^,)|(,$)", "", string)
+        return string
+        
 
 class synonymer_dot_se(synonym_website):
     def __init__(self):
         super(synonymer_dot_se, self).__init__()
+        self.print_next_end_tag = False
 
     def synonym_url(self, word):
         return r"https://www.synonymer.se/sv-syn/"+word
     
-    def lookup_synonym(self, word):
+    def lookup_word(self, word):
         url = self.synonym_url(word)
         website_text = self.fetch_website_text(url)
-        self.read(str(website_text)) #call for website parsing of text
-        return website_text
-
+        self.soup = BeautifulSoup(website_text, "html.parser")
+        synonyms = self.lookup_synonyms()
+        return synonyms
+    
+    def lookup_synonyms(self):
+        dictResult = self.soup.find("div", class_="DictResult")
+        dictDefault = dictResult.find("div", id="dict-default")
+        dictDefaultBodyText = dictDefault.find("div", class_="body").get_text()
+        synonyms = self.strip_whitespaces(dictDefaultBodyText)
+        return synonyms
+        #print(website_parsed)
+    
 word = "flor"
 synonymer_se = synonymer_dot_se()
-synonyms = synonymer_se.lookup_synonym(word)
+synonyms = synonymer_se.lookup_word(word)
 print(synonyms)
+#print(synonyms)
 
